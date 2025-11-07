d16769248b3f7ff3c009ab9cb60596e559fa3161
Tithi Patel <tithiamitpatel@yahoo.com>
2025-11-07 16:59:39 -0500

    Added top description

--- EduMark.py
+++ EduMark.py
@@ -1,3 +1,10 @@
 """
-Write a mini README
+Description: This module provides functionalities for managing educational marks for students.
+It includes classes and methods to add, update, retrieve, and calculate average marks.
+
+Programmer: Tithi Patel
+
+Pre-conditions: Require the word "test" or "final"
+Post-conditions: Returns a treeview that can add, update, or remove a test & students from the files.
+
 """
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # EduMark
 A Python-based tool for teachers to manage and edit student marks easily.
+
+EduMark is a safe and easy site for teachers to track their student's grades. 
