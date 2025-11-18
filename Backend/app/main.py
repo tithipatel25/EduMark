9b17b6e0cc63c135c121062f8bdc6d7db3f9f919
Tithi Patel <tithiamitpatel@yahoo.com>
2025-11-17 23:58:48 -0500

    Testing fastapi

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+class Student(BaseModel):
+    name: str # Student's name (string)
+    subject: str # Subject name (string)
+    marks: float # Marks obtained (float number)
 
-def read_root():
-    return {"message" : "EduMark API is running!"}
+
+#Testing
+@app.get("/")
+def home():
+    return {"message": "Welcome to the Student Marks API"}
+
+
+students_db = [] #temporary list acting like a database
+
+@app.post("/add_mark/") #frontend will send data to this endpoint
+def add_mark(student: Student): #FastAPI will automatically read the incoming JSON to Student model
+    students_db.append(student)
+    return {"message": "Student marks added successfully", "data": student}
