a0864974b574a7d9a6160bd3036700b7bb835e9d
Tithi Patel <tithiamitpatel@yahoo.com>
2025-11-18 00:48:42 -0500

    Frontend changes

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,1 +1,26 @@
-console.log("Frontend app initialized!");
+const form = document.getElementById('marksForm');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const student = {
+        name: document.getElementById('name').value,
+        subject: document.getElementById('subject').value,
+        marks: parseFloat(document.getElementById('marks').value, 10)
+    };
+
+    try {
+        const res = await fetch("http://127.0.0.1:8000/add_mark/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(student)
+        });
+
+        const data = await res.json();
+        document.getElementById('response').innerText = JSON.stringify(data, null, 2);
+    } catch (error) {
+        document.getElementById('response').innerText = 'Error: ' + error;
+    }
+});
